f38b83879a1351b03ab765d1890f7f05f5c9578a
ravitejachelikani <ravitejachelikani@gmail.com>
2020-11-23 19:12:00 +0530

    newipchanged

--- App/config.js
+++ App/config.js
@@ -6,7 +6,13 @@ export const API_URL_FRONT = HOST_FRONT+':8080/o/greetings/';*/
 
 //PROD//
 
-export const HOST = 'http://travelapp.emamigroup.com';
+/*export const HOST = 'http://travelapp.emamigroup.com';
 export const API_URL = HOST+':9092/';
 export const HOST_FRONT = 'http://travelprd.emamigroup.com';
+export const API_URL_FRONT = HOST_FRONT+':8080/o/greetings/';*/
+
+
+export const HOST = 'http://20.40.46.52';
+export const API_URL = HOST+':9092/';
+export const HOST_FRONT = 'http://20.40.45.250';
 export const API_URL_FRONT = HOST_FRONT+':8080/o/greetings/';
